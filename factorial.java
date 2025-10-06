25ce8bd0a848860f8229805e6416a5f467e230d1
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2025-10-06 20:16:23 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,17 @@
+public class factorial{
+    public static int fact(int n)
+    {
+        if(n == 0)
+        return 1;
+        else
+        return n*fact(n-1);
+    }
+    public static void main(String[] args)
+    {
+        int n = 5;
+
+        int x = fact(n);
+        System.out.println(x);
+
+    }
+}
